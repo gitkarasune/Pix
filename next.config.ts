203a8b888c7b44b5421dc87da415a727304ffe1e
unknown <sunebarikara@gmail.com>
2025-10-09 02:40:21 +0100

    fixed:analysing

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
-import type { Configuration as WebpackDevMiddlewareConfig } from "webpack-dev-middleware";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  webpackDevMiddleware: (config: WebpackDevMiddlewareConfig) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
   images: {
     remotePatterns: [
       {
