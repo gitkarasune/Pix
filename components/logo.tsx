3ef87d39cd9fe3f6325305981305368816bb9ad5
unknown <sunebarikara@gmail.com>
2025-10-09 12:01:09 +0100

    adds metadata

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,24 @@
 "use client"
 
 import { cn } from "@/lib/utils"
+import Image from "next/image"
 
 export default function Logo() {
   return (
     <div className={cn("flex items-center ")}>
-      <div
+      {/* <div
       className="relative flex h-2 w-2 dark:bg-white bg-black text-white dark:text-black items-center justify-center rounded-full"
-      >
-        
-      </div>
+      > 
+      </div> */}
+      <Image
+        src="/pix-logo-white.png"
+        alt="Pix Logo"
+        width={24}
+        height={24}
+        className="ml-2 dark:invert"
+        priority
+      />
+
       <span className="text-xl font-bold">
         Pix
       </span>
